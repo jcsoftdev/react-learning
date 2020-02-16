edba807ed36ffa96cbfe596ac726668cca3a47ce
Juan Carlos <ajcarlos032@gmail.com>
2020-02-16 01:26:27 -0500

    prod

---
+++ docs/precache-manifest.25d33b5fb33f9d188c75072b05d4b735.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33adfa68efa22fbd32e2f46211cacda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afd66bb09f02079860b1",
+    "url": "/static/css/main.083bf22c.chunk.css"
+  },
+  {
+    "revision": "b860134c32b89df2954c",
+    "url": "/static/js/2.d25bf836.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d25bf836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd66bb09f02079860b1",
+    "url": "/static/js/main.7c0eee47.chunk.js"
+  },
+  {
+    "revision": "9dcd3c36e306944c9f68",
+    "url": "/static/js/runtime-main.1d76f3d8.js"
+  },
+  {
+    "revision": "6d527aa4ca9684acd48f3aab02e052db",
+    "url": "/static/media/RUTH.6d527aa4.svg"
+  },
+  {
+    "revision": "01c0e8e755abf96cae241b2e32594c5f",
+    "url": "/static/media/TE AMO.01c0e8e7.svg"
+  }
+]);
